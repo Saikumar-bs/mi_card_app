902b1ee24dde72cfc40e9e63b038746dfebb8fe0
SAI KUMAR MISALA <saikumar@bluesapience.com>
2022-04-06 16:09:34 +0530

    test name and position text

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,9 +9,29 @@ testWidgets('To test icons', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
+    // Verify that test icons exist.
     expect(find.byIcon(Icons.phone), findsOneWidget);
     expect(find.byIcon(Icons.email), findsOneWidget);
     
+  });
+
+
+testWidgets('To test text name', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MyApp());
+
+    // find the text Sai Kumar Meesala.
+    final textfinder = find.text('Sai Kumar Meesala');
+
+    expect(textfinder, findsOneWidget);
+  });
+
+testWidgets('To test text position', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MyApp());
+
+    // find the text FLUTTER DEVELOPER.
+    final positionfinder = find.text('FLUTTER DEVELOPER');
+    expect(positionfinder, findsOneWidget);
   });
 }
